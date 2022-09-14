dc441f0f1037cc9a3463ac882d300c0f8fe05a06
Andreas Farah <andreasfarah@gmail.com>
2022-09-14 15:14:08 +0200

    fix: CompilesIconsTest

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -17,11 +17,11 @@ class CompilesIconsTest extends TestCase
 
         // Note: the empty class here seems to be a Blade components bug.
         $expected = <<<SVG
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-accessible" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <circle cx="12" cy="12" r="9"></circle>
-        <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1"></path>
-        <circle cx="12" cy="7.5" r=".5" fill="currentColor"></circle>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-accessible" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="12" r="9" />
+            <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
+            <circle cx="12" cy="7.5" r=".5" fill="currentColor" />
         </svg>
         SVG;
 
@@ -34,11 +34,13 @@ class CompilesIconsTest extends TestCase
         $result = svg('tabler-accessible', 'w-6 h-6 text-gray-500')->toHtml();
 
         $expected = <<<SVG
-        <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <circle cx="12" cy="12" r="9" />
-          <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
-          <circle cx="12" cy="7.5" r=".5" fill="currentColor" />
+        <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-accessible" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="12" r="9" />
+            <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
+            <circle cx="12" cy="7.5" r=".5" fill="currentColor" />
         </svg>
+      
         SVG;
 
         $this->assertStringMatchesFormat($expected, $result);
@@ -50,11 +52,11 @@ class CompilesIconsTest extends TestCase
         $result = svg('tabler-accessible', ['style' => 'color: #555'])->toHtml();
 
         $expected = <<<SVG
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-accessible" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <circle cx="12" cy="12" r="9"></circle>
-        <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1"></path>
-        <circle cx="12" cy="7.5" r=".5" fill="currentColor"></circle>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-accessible" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="12" r="9" />
+            <path d="M10 16.5l2 -3l2 3m-2 -3v-2l3 -1m-6 0l3 1" />
+            <circle cx="12" cy="7.5" r=".5" fill="currentColor" />
         </svg>
         SVG;
 
