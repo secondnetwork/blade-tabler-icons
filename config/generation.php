de743a9eb1666696732eb83c672bd1af345a2aa1
Andy <github@secondnetwork.de>
2024-03-12 14:13:27 +0100

    Update generation.php

--- config/generation.php
+++ config/generation.php
@@ -1,8 +1,15 @@
 <?php
 
 return [
-    [
-        'source' => __DIR__.'/../node_modules/@tabler/icons/icons',
+    [ // Regular Outline...
+        'source' => __DIR__.'/../node_modules/@tabler/icons/icons/outline',
         'destination' => __DIR__.'/../resources/svg',
+        'safe' => true,
+    ],
+    [ // Regular filled...
+        'source' => __DIR__.'/../node_modules/@tabler/icons/icons/filled',
+        'destination' => __DIR__.'/../resources/svg',
+        'safe' => true,
     ],
 ];
+
